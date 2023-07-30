f817df00dd5b7f8c60d542bf42b6c8f137cfc673
SsuJo_ <1049731887@qq.com>
2023-07-30 23:32:48 +0800

    深夜 大动干戈

--- img/work/change图片为渐进式jpeg.py
+++ img/work/change图片为渐进式jpeg.py
@@ -5,18 +5,23 @@ def batch_convert_to_progressive_jpeg(source_folder, destination_folder):
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
 
+    lese_folder = os.path.join(source_folder, "lese")
+    if not os.path.exists(lese_folder):
+        os.makedirs(lese_folder)
+
     for filename in os.listdir(source_folder):
         if filename.lower().endswith(".jpg"):
             source_path = os.path.join(source_folder, filename)
             destination_path = os.path.join(destination_folder, filename)
 
-            # Open the image and convert it to progressive JPEG
-            with Image.open(source_path) as img:
-                # Save the image as a progressive JPEG
-                img.save(destination_path, "JPEG", quality=100, progressive=True)
+            # Move the original image to "lese" folder
+            lese_path = os.path.join(lese_folder, filename)
+            os.rename(source_path, lese_path)
 
-            # Remove the original non-progressive JPEG
-            os.remove(source_path)
+            # Open the image and convert it to progressive JPEG
+            with Image.open(lese_path) as img:
+                # Save the image as a progressive JPEG, overwriting the original
+                img.save(destination_path, "JPEG", quality=100, progressive=True)
 
 if __name__ == "__main__":
     source_folder = "."
