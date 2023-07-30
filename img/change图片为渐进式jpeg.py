1b820c075e6fde4be9a075a234bfc65bad542d46
SsuJo_ <1049731887@qq.com>
2023-07-30 22:02:51 +0800

    更新图片

--- img/change图片为渐进式jpeg.py
+++ img/change图片为渐进式jpeg.py
@@ -1,13 +1,25 @@
+import os
 from PIL import Image
-from PIL import ImageFile
 
-img_path = "p3.jpg"  # Make sure this path is correct
-destination = "p3.jpeg"
+def batch_convert_to_progressive_jpeg(source_folder, destination_folder):
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
 
-try:
-    with Image.open(img_path) as img:
-        img.save(destination, "JPEG", quality=80, optimize=True, progressive=True)
-except IOError:
-    ImageFile.MAXBLOCK = img.size[0] * img.size[1]
-    with Image.open(img_path) as img:
-        img.save(destination, "JPEG", quality=80, optimize=True, progressive=True)
+    for filename in os.listdir(source_folder):
+        if filename.lower().endswith(".jpg"):
+            source_path = os.path.join(source_folder, filename)
+            destination_path = os.path.join(destination_folder, filename)
+
+            # Open the image and convert it to progressive JPEG
+            with Image.open(source_path) as img:
+                # Save the image as a progressive JPEG
+                img.save(destination_path, "JPEG", quality=100, progressive=True)
+
+            # Remove the original non-progressive JPEG
+            os.remove(source_path)
+
+if __name__ == "__main__":
+    source_folder = "."
+    destination_folder = os.path.join(source_folder, "lese")
+
+    batch_convert_to_progressive_jpeg(source_folder, destination_folder)
---
+++ img/检查渐进式jpeg.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import os
+
+def is_progressive_jpeg(image_path):
+    try:
+        with Image.open(image_path) as img:
+            return "progressive" in img.info and img.info["progressive"]
+    except IOError:
+        return False
+
+if __name__ == "__main__":
+    image_name = input("请输入图片名（包括文件扩展名）：")
+    image_path = os.path.join(".", image_name)
+
+    if is_progressive_jpeg(image_path):
+        print("这是渐进式JPEG图片。")
+    else:
+        print("这不是渐进式JPEG图片。")
