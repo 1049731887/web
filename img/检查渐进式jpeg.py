59a94d03a02dc4feff735ef0d786b123f9eb3dd2
SsuJo_ <1049731887@qq.com>
2023-08-08 17:40:08 +0800

    格式

--- img/检查渐进式jpeg.py
+++ img/检查渐进式jpeg.py
@@ -1,12 +1,14 @@
 from PIL import Image
 import os
 
+
 def is_progressive_jpeg(image_path):
     try:
         with Image.open(image_path) as img:
             return "progressive" in img.info and img.info["progressive"]
     except IOError:
         return False
+
 
 if __name__ == "__main__":
     image_name = input("请输入图片名（包括文件扩展名）：")
